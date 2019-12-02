fdccc707c0903c24abe4476a0ce21b64109b925f
perosa <beppe.catanese@gmail.com>
2019-12-02 22:17:52 +0100

    Test context parsing

---
+++ src/test/java/com/perosa/avatarbot/payload/ContextParserTest.java
@@ -0,0 +1,56 @@
+package com.perosa.avatarbot.payload;
+
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.dialogflow.v2.model.GoogleCloudDialogflowV2WebhookRequest;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Logger;
+
+import static org.junit.Assert.*;
+
+public class ContextParserTest {
+
+    private static final Logger LOGGER = Logger.getLogger(ContextParserTest.class.getName());
+
+    @Test
+    public void isWelcome() {
+        LOGGER.info("isWelcome");
+
+        GoogleCloudDialogflowV2WebhookRequest request = load("/webhook1.json");
+
+        ContextParser helper = new ContextParser();
+
+        assertEquals(true, helper.isWelcomeIntent(request));
+    }
+
+    @Test
+    public void getContext() {
+        LOGGER.info("getContext");
+
+        GoogleCloudDialogflowV2WebhookRequest request = load("/webhook1.json");
+
+        ContextParser helper = new ContextParser();
+
+        assertNotNull(helper.getContext(request, "projects/brandmybot-qaqudq/agent/sessions/01/contexts/DefaultWelcomeIntent-followup"));
+    }
+
+
+    private GoogleCloudDialogflowV2WebhookRequest load(String filename) {
+
+        GoogleCloudDialogflowV2WebhookRequest request;
+
+        InputStream inputStream = getClass().getResourceAsStream(filename);
+
+        try {
+            request = JacksonFactory.getDefaultInstance().createJsonParser(inputStream)
+                    .parse(GoogleCloudDialogflowV2WebhookRequest.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return request;
+    }
+
+}
